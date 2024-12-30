2f982f3dd0593b37becbf049a1136522ef776ec5
DDWe11 <Duanjw2002@163.com>
2024-12-30 21:15:44 +0800

    学生管理部分功能基本完善

--- client/src/views/Admin/studentManage/addStudent.vue
+++ client/src/views/Admin/studentManage/addStudent.vue
@@ -1,56 +1,61 @@
 <template>
   <div>
-    <el-form
-        style="width: 60%; margin: 20px auto;"
-        :model="ruleForm"
-        :rules="rules"
-        ref="ruleForm"
-        label-width="120px"
-        class="demo-ruleForm"
+    <div
+        style="
+        width: 90%;
+        margin: 50px auto;
+        padding: 40px;
+        border: 3px solid #dcdcdc;
+        box-shadow: 0 6px 15px rgba(0, 0, 0, 0.2);
+        border-radius: 15px;
+        background-color: #ffffff;
+      "
     >
-      <el-row :gutter="20">
-        <el-col :span="12">
+      <h2 style="text-align: left; margin-bottom: 40px; font-size: 24px;">新增学生信息</h2>
+      <el-form
+          :model="ruleForm"
+          :rules="rules"
+          ref="ruleForm"
+          label-width="120px"
+          style="font-size: 16px;"
+      >
+        <div
+            style="
+            display: grid;
+            grid-template-columns: repeat(2, 1fr);
+            gap: 20px 30px;
+          "
+        >
+          <!-- 第一列 -->
           <el-form-item label="学生ID" prop="sid">
-            <el-input v-model="ruleForm.sid"></el-input>
+            <el-input
+                v-model="ruleForm.sid"
+                placeholder="请输入学生ID"
+                style="width: 260px; height: 40px; font-size: 15px;"
+            ></el-input>
           </el-form-item>
-        </el-col>
-        <el-col :span="12">
-          <el-form-item label="学生姓名" prop="sname">
-            <el-input v-model="ruleForm.sname"></el-input>
+          <el-form-item label="密码" prop="password">
+            <el-input
+                v-model="ruleForm.password"
+                show-password
+                placeholder="请输入密码"
+                style="width: 260px; height: 40px; font-size: 15px;"
+            ></el-input>
           </el-form-item>
-        </el-col>
-      </el-row>
-      <el-row :gutter="20">
-        <el-col :span="12">
-          <el-form-item label="初始密码" prop="password">
-            <el-input v-model="ruleForm.password" show-password></el-input>
+          <!-- 第二列 -->
+          <el-form-item label="姓名" prop="sname">
+            <el-input
+                v-model="ruleForm.sname"
+                placeholder="请输入学生姓名"
+                style="width: 260px; height: 40px; font-size: 15px;"
+            ></el-input>
           </el-form-item>
-        </el-col>
-        <el-col :span="12">
-          <el-form-item label="校区" prop="campus">
-            <el-select v-model="ruleForm.campus" placeholder="请选择校区">
-              <el-option label="南校区" value="南校区"></el-option>
-              <el-option label="北校区" value="北校区"></el-option>
-            </el-select>
-          </el-form-item>
-        </el-col>
-      </el-row>
-      <el-row :gutter="20">
-        <el-col :span="12">
-          <el-form-item label="学院" prop="college">
-            <el-select v-model="ruleForm.college" placeholder="请选择学院">
-              <el-option
-                  v-for="college in colleges"
-                  :key="college"
-                  :label="college"
-                  :value="college"
-              ></el-option>
-            </el-select>
-          </el-form-item>
-        </el-col>
-        <el-col :span="12">
           <el-form-item label="民族" prop="ethnicity">
-            <el-select v-model="ruleForm.ethnicity" placeholder="请选择民族">
+            <el-select
+                v-model="ruleForm.ethnicity"
+                placeholder="请选择民族"
+                style="width: 260px; height: 40px; font-size: 15px;"
+            >
               <el-option
                   v-for="ethnicity in ethnicities"
                   :key="ethnicity"
@@ -59,49 +64,88 @@
               ></el-option>
             </el-select>
           </el-form-item>
-        </el-col>
-      </el-row>
-      <el-row :gutter="20">
-        <el-col :span="12">
+          <!-- 第三列 -->
           <el-form-item label="身份证号" prop="idNumber">
-            <el-input v-model="ruleForm.idNumber"></el-input>
+            <el-input
+                v-model="ruleForm.idNumber"
+                placeholder="请输入身份证号"
+                style="width: 260px; height: 40px; font-size: 15px;"
+            ></el-input>
           </el-form-item>
-        </el-col>
-        <el-col :span="12">
           <el-form-item label="政治面貌" prop="politicalStatus">
-            <el-select v-model="ruleForm.politicalStatus" placeholder="请选择政治面貌">
+            <el-select
+                v-model="ruleForm.politicalStatus"
+                placeholder="请选择政治面貌"
+                style="width: 260px; height: 40px; font-size: 15px;"
+            >
               <el-option label="党员" value="党员"></el-option>
               <el-option label="团员" value="团员"></el-option>
               <el-option label="群众" value="群众"></el-option>
             </el-select>
           </el-form-item>
-        </el-col>
-      </el-row>
-      <el-row :gutter="20">
-        <el-col :span="12">
+          <!-- 第四列 -->
+          <el-form-item label="校区" prop="campus">
+            <el-select
+                v-model="ruleForm.campus"
+                placeholder="请选择校区"
+                style="width: 260px; height: 40px; font-size: 15px;"
+            >
+              <el-option label="南校区" value="南校区"></el-option>
+              <el-option label="北校区" value="北校区"></el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item label="学院" prop="college">
+            <el-select
+                v-model="ruleForm.college"
+                placeholder="请选择学院"
+                style="width: 260px; height: 40px; font-size: 15px;"
+            >
+              <el-option
+                  v-for="college in colleges"
+                  :key="college"
+                  :label="college"
+                  :value="college"
+              ></el-option>
+            </el-select>
+          </el-form-item>
+          <!-- 第五列 -->
+          <el-form-item label="专业" prop="subject">
+            <el-input
+                v-model="ruleForm.subject"
+                placeholder="请输入专业"
+                style="width: 260px; height: 40px; font-size: 15px;"
+            ></el-input>
+          </el-form-item>
           <el-form-item label="年级" prop="grade">
-            <el-select v-model="ruleForm.grade" placeholder="请选择年级">
+            <el-select
+                v-model="ruleForm.grade"
+                placeholder="请选择年级"
+                style="width: 260px; height: 40px; font-size: 15px;"
+            >
               <el-option
                   v-for="year in years"
                   :key="year"
                   :label="`${year}级`"
-                  :value="year"
+                  :value="`${year}级`"
               ></el-option>
             </el-select>
           </el-form-item>
-        </el-col>
-        <el-col :span="12">
-          <el-form-item label="专业" prop="subject">
-            <el-input v-model="ruleForm.subject"></el-input>
-          </el-form-item>
-        </el-col>
-      </el-row>
-      <el-form-item style="text-align: center;">
-        <el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>
-        <el-button @click="resetForm('ruleForm')">重置</el-button>
-        <el-button @click="test">test</el-button>
-      </el-form-item>
-    </el-form>
+        </div>
+        <!-- 按钮 -->
+        <el-form-item style="text-align: center; margin-top: 40px; margin-left: -80px;">
+          <el-button type="primary" @click="submitForm('ruleForm')" style="padding: 12px 30px;">
+            提交
+          </el-button>
+          <el-button
+              @click="resetForm('ruleForm')"
+              style="padding: 12px 30px; margin-left: 30px;"
+          >
+            重置
+          </el-button>
+          <el-button @click="test" style="padding: 12px 30px; margin-left: 30px;">test</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
   </div>
 </template>
 
@@ -132,6 +176,11 @@ export default {
         ethnicity: [{ required: true, message: "请选择民族", trigger: "change" }],
         idNumber: [
           { required: true, message: "请输入身份证号", trigger: "blur" },
+          // {
+          //   pattern: /^[1-9]\d{16}[0-9Xx]$/,
+          //   message: "请输入有效的身份证号",
+          //   trigger: "blur",
+          // },
         ],
         politicalStatus: [
           { required: true, message: "请选择政治面貌", trigger: "change" },
--- client/src/views/Admin/studentManage/editorStudent.vue
+++ client/src/views/Admin/studentManage/editorStudent.vue
@@ -1,49 +1,152 @@
 <template>
   <div>
-    <el-form style="width: 60%" :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
-      <!-- 学生姓名 -->
-      <el-form-item label="学生姓名" prop="sname">
-        <el-input v-model="ruleForm.sname"></el-input>
-      </el-form-item>
-      <!-- 初始密码 -->
-      <el-form-item label="初始密码" prop="password">
-        <el-input type="password" v-model="ruleForm.password"></el-input>
-      </el-form-item>
-      <!-- 学院 -->
-      <el-form-item label="学院" prop="college">
-        <el-input v-model="ruleForm.college"></el-input>
-      </el-form-item>
-      <!-- 民族 -->
-      <el-form-item label="民族" prop="ethnicity">
-        <el-input v-model="ruleForm.ethnicity"></el-input>
-      </el-form-item>
-      <!-- 身份证号 -->
-      <el-form-item label="身份证号" prop="idNumber">
-        <el-input v-model="ruleForm.idNumber"></el-input>
-      </el-form-item>
-      <!-- 政治面貌 -->
-      <el-form-item label="政治面貌" prop="politicalStatus">
-        <el-input v-model="ruleForm.politicalStatus"></el-input>
-      </el-form-item>
-      <!-- 年级 -->
-      <el-form-item label="年级" prop="grade">
-        <el-input v-model="ruleForm.grade"></el-input>
-      </el-form-item>
-      <!-- 专业 -->
-      <el-form-item label="专业" prop="subject">
-        <el-input v-model="ruleForm.subject"></el-input>
-      </el-form-item>
-      <!-- 校区 -->
-      <el-form-item label="校区" prop="campus">
-        <el-input v-model="ruleForm.campus"></el-input>
-      </el-form-item>
-      <!-- 提交与重置按钮 -->
-      <el-form-item>
-        <el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>
-        <el-button @click="resetForm('ruleForm')">重置</el-button>
-        <el-button @click="test">测试</el-button>
-      </el-form-item>
-    </el-form>
+    <div
+        style="
+        width: 90%;
+        margin: 50px auto;
+        padding: 50px;
+        border: 3px solid #dcdcdc;
+        box-shadow: 0 8px 20px rgba(0, 0, 0, 0.2);
+        border-radius: 15px;
+        background-color: #ffffff;
+      "
+    >
+      <h2 style="text-align: left; margin-bottom: 40px; font-size: 24px;">修改学生信息</h2>
+      <el-form
+          :model="ruleForm"
+          :rules="rules"
+          ref="ruleForm"
+          label-width="120px"
+          style="font-size: 16px;"
+      >
+        <div
+            style="
+            display: grid;
+            grid-template-columns: repeat(2, 1fr);
+            gap: 30px 40px;
+          "
+        >
+          <!-- 第一列 -->
+          <el-form-item label="学生ID" prop="sid">
+            <el-input
+                v-model="ruleForm.sid"
+                placeholder="请输入学生ID"
+                style="width: 280px; height: 40px; font-size: 15px;"
+                disabled
+            ></el-input>
+          </el-form-item>
+          <el-form-item label="密码" prop="password">
+            <el-input
+                v-model="ruleForm.password"
+                show-password
+                placeholder="请输入密码"
+                style="width: 280px; height: 40px; font-size: 15px;"
+            ></el-input>
+          </el-form-item>
+          <!-- 第二列 -->
+          <el-form-item label="姓名" prop="sname">
+            <el-input
+                v-model="ruleForm.sname"
+                placeholder="请输入学生姓名"
+                style="width: 280px; height: 40px; font-size: 15px;"
+            ></el-input>
+          </el-form-item>
+          <el-form-item label="民族" prop="ethnicity">
+            <el-select
+                v-model="ruleForm.ethnicity"
+                placeholder="请选择民族"
+                style="width: 280px; height: 40px; font-size: 15px;"
+            >
+              <el-option
+                  v-for="ethnicity in ethnicities"
+                  :key="ethnicity"
+                  :label="ethnicity"
+                  :value="ethnicity"
+              ></el-option>
+            </el-select>
+          </el-form-item>
+          <!-- 第三列 -->
+          <el-form-item label="身份证号" prop="idNumber">
+            <el-input
+                v-model="ruleForm.idNumber"
+                placeholder="请输入身份证号"
+                style="width: 280px; height: 40px; font-size: 15px;"
+            ></el-input>
+          </el-form-item>
+          <el-form-item label="政治面貌" prop="politicalStatus">
+            <el-select
+                v-model="ruleForm.politicalStatus"
+                placeholder="请选择政治面貌"
+                style="width: 280px; height: 40px; font-size: 15px;"
+            >
+              <el-option label="党员" value="党员"></el-option>
+              <el-option label="团员" value="团员"></el-option>
+              <el-option label="群众" value="群众"></el-option>
+            </el-select>
+          </el-form-item>
+          <!-- 第四列 -->
+          <el-form-item label="校区" prop="campus">
+            <el-select
+                v-model="ruleForm.campus"
+                placeholder="请选择校区"
+                style="width: 280px; height: 40px; font-size: 15px;"
+            >
+              <el-option label="南校区" value="南校区"></el-option>
+              <el-option label="北校区" value="北校区"></el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item label="学院" prop="college">
+            <el-select
+                v-model="ruleForm.college"
+                placeholder="请选择学院"
+                style="width: 280px; height: 40px; font-size: 15px;"
+            >
+              <el-option
+                  v-for="college in colleges"
+                  :key="college"
+                  :label="college"
+                  :value="college"
+              ></el-option>
+            </el-select>
+          </el-form-item>
+          <!-- 第五列 -->
+          <el-form-item label="专业" prop="subject">
+            <el-input
+                v-model="ruleForm.subject"
+                placeholder="请输入专业"
+                style="width: 280px; height: 40px; font-size: 15px;"
+            ></el-input>
+          </el-form-item>
+          <el-form-item label="年级" prop="grade">
+            <el-select
+                v-model="ruleForm.grade"
+                placeholder="请选择年级"
+                style="width: 260px; height: 40px; font-size: 15px;"
+            >
+              <el-option
+                  v-for="year in years"
+                  :key="year"
+                  :label="`${year}级`"
+                  :value="`${year}级`"
+              ></el-option>
+            </el-select>
+          </el-form-item>
+        </div>
+        <!-- 按钮 -->
+        <el-form-item style="text-align: center; margin-top: 40px; margin-left: -50px;">
+          <el-button type="primary" @click="submitForm('ruleForm')" style="padding: 12px 30px;">
+            提交
+          </el-button>
+          <el-button
+              @click="resetForm('ruleForm')"
+              style="padding: 12px 30px; margin-left: 30px;"
+          >
+            重置
+          </el-button>
+          <el-button @click="test" style="padding: 12px 30px; margin-left: 30px;">测试</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
   </div>
 </template>
 
@@ -66,6 +169,25 @@ export default {
         subject: null,
         campus: null,
       },
+      ethnicities: ["汉族", "蒙古族", "回族", "藏族", "维吾尔族", "苗族", "彝族", "壮族", "布依族", "朝鲜族",
+        "满族", "侗族", "瑶族", "白族", "土家族", "哈尼族", "哈萨克族", "傣族", "黎族", "僳僳族",
+        "佤族", "畲族", "高山族", "拉祜族", "水族", "东乡族", "纳西族", "景颇族", "柯尔克孜族",
+        "土族", "达斡尔族", "仫佬族", "羌族", "布朗族", "撒拉族", "毛南族", "仡佬族", "锡伯族",
+        "阿昌族", "普米族", "塔吉克族", "怒族", "乌孜别克族", "俄罗斯族", "鄂温克族", "德昂族",
+        "保安族", "裕固族", "京族", "塔塔尔族", "独龙族", "鄂伦春族", "赫哲族", "门巴族",
+        "珞巴族", "基诺族",],
+      years: [2018, 2019, 2020, 2021, 2022, 2023, 2024],
+      colleges: [
+        "计算机学院",
+        "软件学院",
+        "法学院",
+        "商学院",
+        "文学与新闻传播学院",
+        "外国语学院",
+        "数学与统计学院",
+        "物理与电子学院",
+        "化学与材料学院",
+        "生命科学学院",],
       rules: {
         sname: [
           { required: true, message: '请输入学生姓名', trigger: 'blur' },
@@ -99,22 +221,27 @@ export default {
     };
   },
   created() {
-    // 获取学生 ID
     if (this.$route.query.sid === undefined) {
       this.ruleForm.sid = 1;
     } else {
       this.ruleForm.sid = this.$route.query.sid;
     }
-    // 根据 ID 获取学生信息
+
     axios.get(`${BASE_URL}/student/findById/${this.ruleForm.sid}`).then((resp) => {
-      Object.assign(this.ruleForm, resp.data); // 合并返回的数据
+      const studentData = resp.data;
+
+      // 格式化年级
+      if (studentData.grade && !studentData.grade.includes("级")) {
+        studentData.grade += "级";
+      }
+
+      Object.assign(this.ruleForm, studentData);
     });
   },
   methods: {
     submitForm(formName) {
       this.$refs[formName].validate((valid) => {
         if (valid) {
-          console.log("当前表单数据：", this.ruleForm);
           axios.post(`${BASE_URL}/student/updateStudent`, this.ruleForm)
               .then((resp) => {
                 if (resp.data === true) {
@@ -130,9 +257,10 @@ export default {
               })
               .catch((error) => {
                 console.error("请求失败：", error);
-                this.$message.error('请求失败，请检查网络或联系管理员');
+                this.$message.error('请求失败，请检查网络或服务器');
               });
         } else {
+          console.log("校验失败");
           return false;
         }
       });
@@ -141,7 +269,7 @@ export default {
       this.$refs[formName].resetFields();
     },
     test() {
-      console.log("当前表单数据：", this.ruleForm);
+      console.log(this.ruleForm);
     },
   },
 };
